29d599b9c200fa546c679351e3f8e487b7b4f824
Tanya Kryukova <ttkryukova@gmail.com>
2017-08-02 01:40:49 +0000

    using reassignment to reduce number of print() to only 2

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for c in str:
-        if ord(c) > 96 and ord(c) < 123:
-            print("{}".format(chr(ord(c) - 32)), end="")
-        else:
-            print("{}".format(c), end="")
+        if ord(c) >= ord('a') and ord(c) < ord('z'):
+            c = chr(ord(c) - 32)  #reassignment to limit the number of print()
+        print("{}".format(c), end="")
+    print("")
