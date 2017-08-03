2fbfa1c9ad8371d68569f95013c8bc418cf06c6f
Tanya Kryukova <ttkryukova@gmail.com>
2017-08-03 23:33:16 +0000

    first commit

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    sin = "argument"
+    plu = "arguments"
+
+    # 0 arguments
+    if len(sys.argv) == 1:
+        print("{:d} {}.".format(0, sin))
+
+    # 1 argument
+    elif len(sys.argv) == 2:
+        print("{:d} {}:".format(1, sin))
+        print("{:d}: {}".format(1, sys.argv[1]))
+
+    # more than 1 argument
+    else:
+        print("{:d} {}:".format(len(sys.argv) - 1, plu))
+        for i in range(1, len(sys.argv) + 1):
+              print("{:d}: {}".format(i, sys.argv[i - 1]))
