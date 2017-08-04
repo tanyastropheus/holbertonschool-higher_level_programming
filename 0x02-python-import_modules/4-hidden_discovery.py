24da412e153f5c4a8c2a1a6f49be09d1f17a2e3f
Tanya Kryukova <ttkryukova@gmail.com>
2017-08-04 07:27:37 +0000

    used string slicing

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     import hidden_4
     s_alpha = sorted(dir(hidden_4))
     for i in s_alpha:
-        if i[0] != "_" and i[1] != "_":
+        if i[:2] != "__":
             print("{}".format(i), end="\n")
