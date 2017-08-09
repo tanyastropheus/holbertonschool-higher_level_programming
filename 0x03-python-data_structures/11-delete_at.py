e1c7d8d85d8efbba33f1bf0b9b6c50eaf4965242
Tanya Kryukova <ttkryukova@gmail.com>
2017-08-09 07:02:27 +0000

    removed redundant code

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-        if my_list == []:
-                return (my_list)
-        elif idx >= len(my_list):
+        if my_list == [] or idx >= len(my_list):
                 return (my_list)
         else:
                 del my_list[idx]
