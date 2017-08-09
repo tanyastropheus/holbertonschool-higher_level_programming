b76d65aaafb4b577a4ab988c32649dba9b176664
Tanya Kryukova <ttkryukova@gmail.com>
2017-08-09 04:13:34 +0000

    delete redundant code

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,9 +3,8 @@ def print_matrix_integer(matrix=[[]]):
         if matrix == [[]]:
                 print("")
         elif matrix:
-                for i in range(len(matrix)):
-                        row = matrix[i]
-                        for j in row:
+                for row in matrix:
+                        for j in row: 
                                 if j == row[len(row) - 1]:
                                         print("{}".format(j))
                                 else:
