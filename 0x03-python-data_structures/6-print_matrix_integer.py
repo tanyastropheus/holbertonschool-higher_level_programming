1d1dba92dd3d04ffc757cb1127fef7432cadf530
Tanya Kryukova <ttkryukova@gmail.com>
2017-08-09 04:16:55 +0000

    testing

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+
+
 def print_matrix_integer(matrix=[[]]):
         if matrix == [[]]:
                 print("")
         elif matrix:
                 for row in matrix:
-                        for j in row: 
+                        for j in row:
                                 if j == row[len(row) - 1]:
                                         print("{}".format(j))
                                 else:
