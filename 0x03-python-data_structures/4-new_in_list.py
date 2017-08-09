88f23bd04ec0267c226f050683405d80def7a4f9
Tanya Kryukova <ttkryukova@gmail.com>
2017-08-09 01:53:44 +0000

    first commit

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if my_list:
+        new_list = list(my_list)
+        if idx in range(len(new_list)):
+            new_list[idx] = element
+            return (new_list)
