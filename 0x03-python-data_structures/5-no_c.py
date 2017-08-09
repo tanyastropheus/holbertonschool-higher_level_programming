bd666f6ac87eed592221a643788c923c1567c7d3
Tanya Kryukova <ttkryukova@gmail.com>
2017-08-09 03:00:46 +0000

    function accepts empty strings

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if my_string:
         c_list = list(my_string)
         c_list = [i for i in c_list if i != 'c' and i != 'C']
         new_string = ''.join(c_list)
