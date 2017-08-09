0da1640bf38e34a6958f78d2728c8e05656e6836
Tanya Kryukova <ttkryukova@gmail.com>
2017-08-09 03:58:27 +0000

    first commit

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+        if matrix == [[]]:
+                print("")
+        else:
+                for i in range(len(matrix)):
+                        row = matrix[i]
+                        for j in row:
+                                if j == row[len(row) - 1]:
+                                        print("{}".format(j))
+                                else:
+                                        print("{}".format(j), end=" ")
