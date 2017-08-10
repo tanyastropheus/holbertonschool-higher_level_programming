cb598d42d9bfac69c09694b7642962113fcd9cfc
Tanya Kryukova <ttkryukova@gmail.com>
2017-08-10 05:09:47 +0000

    using slicing to create a reversed list

--- 0x03-python-data_structures/3-alt.py
+++ 0x03-python-data_structures/3-alt.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     rev_list = my_list[::-1]
+    print(id(my_list))
+    print(id(rev_list))
     for i in rev_list:
         print("{:d}".format(i))
