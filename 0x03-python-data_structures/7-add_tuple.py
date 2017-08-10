185c9472f273cf7e5ad31e2648ba589d32c2e560
Tanya Kryukova <ttkryukova@gmail.com>
2017-08-10 06:41:11 +0000

    converted the list return to tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,4 +5,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
     list_b = list(tuple_b) + [0, 0]
     for i in range(2):
         sum.append(list_a[i] + list_b[i])
-    return (sum)
+    return (tuple(sum))
