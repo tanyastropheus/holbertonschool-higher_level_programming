17155f5493ee5775fb650c07b76761f37005b2fd
Tanya Kryukova <ttkryukova@gmail.com>
2017-08-10 03:43:50 +0000

    fixed .append() argument to evalaute to boolean

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,5 +2,6 @@
 def divisible_by_2(my_list=[]):
     new_list = []
     for i in my_list:  # i is a copy of the elements in my_list
-        new_list.append(i % 2)  # i % 2 evaluates to boolen (T/F)
+        new_list.append(i % 2 == 0)  #  evaluates to boolen (T/F)
+    print(new_list)
     return (new_list)
