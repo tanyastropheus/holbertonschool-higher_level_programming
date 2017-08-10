d0483d9b25738d316efed7235c2d490224682ded
Tanya Kryukova <ttkryukova@gmail.com>
2017-08-10 03:45:27 +0000

    fixed pep8

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,6 +2,5 @@
 def divisible_by_2(my_list=[]):
     new_list = []
     for i in my_list:  # i is a copy of the elements in my_list
-        new_list.append(i % 2 == 0)  #  evaluates to boolen (T/F)
-    print(new_list)
+        new_list.append(i % 2 == 0)  # evaluates to boolen (T/F)
     return (new_list)
