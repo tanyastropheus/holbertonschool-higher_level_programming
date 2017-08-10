b809cf91a424ac5ce0d06adc09658d2c4404c2cf
Tanya Kryukova <ttkryukova@gmail.com>
2017-08-10 03:39:58 +0000

    more pythonic code

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    mul_2 = list(my_list)
-    if my_list == []:
-        return (mul_2)
-    else:
-        for i in range(len(mul_2)):
-            if mul_2[i] % 2 == 0:
-                mul_2[i] = True
-            else:
-                mul_2[i] = False
-    return (mul_2)
+    new_list = []
+    for i in my_list:  # i is a copy of the elements in my_list
+        new_list.append(i % 2)  # i % 2 evaluates to boolen (T/F)
+    return (new_list)
