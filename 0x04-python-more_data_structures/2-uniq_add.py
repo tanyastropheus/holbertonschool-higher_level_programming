c18d57abcea499a0298f09994c98207d4da8281b
Tanya Kryukova <ttkryukova@gmail.com>
2017-08-11 01:40:45 +0000

    using built-in functions

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,13 +1,3 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    if len(my_list) == 0:
-        return (0)
-    else:
-        new_list = sorted(my_list)
-        sum = new_list[0]
-        num = new_list[0]
-        for i in new_list:
-            if i != num:
-                sum = sum + i
-                num = i
-    return (sum)
+    return (sum(set(my_list)))
