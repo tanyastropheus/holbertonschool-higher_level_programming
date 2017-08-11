bfcc1a3f9997fb133946347bd9f4266fbebd4afa
Tanya Kryukova <ttkryukova@gmail.com>
2017-08-11 06:42:41 +0000

    fixed edge case condition

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(my_dict):
-    if my_dict is None:
+    if not my_dict:
         return (None)
     flip_dict = {v: k for (k, v) in my_dict.items()}
-    max_value = flip_dict[max((flip_dict).keys())]
+    max_value = flip_dict[max(flip_dict.keys())]
     return (max_value)
