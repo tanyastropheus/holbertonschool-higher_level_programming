ad0d52867be9cc3cbab1353f39201f57751bf298
Tanya Kryukova <ttkryukova@gmail.com>
2017-08-13 05:01:17 +0000

    first commit

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def complex_delete(my_dict, value):
+        if not my_dict:
+                return (None)
+	for (k,v) in list(my_dict.items()):
+		if v == value:
+			del my_dict[k]
+	return (my_dict)
