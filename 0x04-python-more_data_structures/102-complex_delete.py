3abd5aed66f296ce03629b5561095056704fb86a
Tanya Kryukova <ttkryukova@gmail.com>
2017-08-13 05:07:16 +0000

    fixed pep8

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def complex_delete(my_dict, value):
-        if not my_dict:
-                return (None)
-	for (k,v) in list(my_dict.items()):
-		if v == value:
-			del my_dict[k]
-	return (my_dict)
+    if not my_dict:
+        return (None)
+    for (k, v) in list(my_dict.items()):
+        if v == value:
+            del my_dict[k]
+    return (my_dict)
