6c1c0b32e3b4c6930de2bbfaf302482cff5b14ae
Tanya Kryukova <ttkryukova@gmail.com>
2017-08-13 05:14:17 +0000

    deleted unnecessary edge case

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def complex_delete(my_dict, value):
-    if not my_dict:
-        return (None)
     for (k, v) in list(my_dict.items()):
         if v == value:
             del my_dict[k]
