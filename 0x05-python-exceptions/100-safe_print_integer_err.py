ffaa64e2f81e66ba774cbf47312eae7613458532
Tanya Kryukova <ttkryukova@gmail.com>
2017-08-24 03:09:08 +0000

    used {}.format in print(), following the problem instruction

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,5 +5,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except ValueError as err:
-        print("Exception:", err, file=sys.stderr)
+        print("Exception: {}".format(err), file=sys.stderr)
         return False
