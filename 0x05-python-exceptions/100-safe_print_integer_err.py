bb7348842e19b0dc4fdeffdb9b4ec39ec00d7544
Tanya Kryukova <ttkryukova@gmail.com>
2017-08-24 03:06:35 +0000

    first commit

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,9 @@
+#!/bin/usr/python3
+def safe_print_integer_err(value):
+    import sys
+    try:
+        print("{:d}".format(value))
+        return True
+    except ValueError as err:
+        print("Exception:", err, file=sys.stderr)
+        return False
