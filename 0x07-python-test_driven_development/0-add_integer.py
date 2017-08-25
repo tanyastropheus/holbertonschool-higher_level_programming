881c97d11c7c877cb667f35ec8a8b043df8436c2
Tanya Kryukova <ttkryukova@gmail.com>
2017-08-25 20:47:09 +0000

    need to add docstring

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add_integer(a, b):
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
+
+#    for op in (a, b):
+#        if type(op) not in [int, float]:
+#            raise TypeError("{} must be an integer".format(a))
+#    return int(a) + int(b)
