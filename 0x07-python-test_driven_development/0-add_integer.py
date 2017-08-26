19290f2bff8c4a4c2aaeac79495fdfa01bbc7ba8
Tanya Kryukova <ttkryukova@gmail.com>
2017-08-26 06:20:42 +0000

    added docstring

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
 def add_integer(a, b):
+    """Function that adds two numbers together
+
+    Args:
+        a (int/float): the first number.
+        b (int/float): the second number.
+
+    Returns:
+        int: sum of a and b
+
+    """
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
