c717b1b08c2c9d64b892a5c4758c7f2934bd6c16
Tanya Kryukova <ttkryukova@gmail.com>
2017-08-29 05:41:10 +0000

    fixed error in the setters for width and height by checking parameter value

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -25,9 +25,9 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if type(self.__width) is not int:
+        if type(value) is not int:
             raise TypeError("width must be an integer")
-        elif self.__width < 0:
+        elif value < 0:
             raise ValueError("width must be >= 0")
         else:
             self.__width = value
@@ -45,9 +45,9 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        if type(self.__height) is not int:
+        if type(value) is not int:
             raise TypeError("height must be an integer")
-        elif self.__height < 0:
+        elif value < 0:
             raise ValueError("height must be >= 0")
         else:
             self.__height = value
