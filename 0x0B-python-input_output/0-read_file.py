aeecab6331321c3a135241644eacf40fde48618a
Tanya Kryukova <ttkryukova@gmail.com>
2017-09-08 01:36:57 +0000

    first commit

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def read_file(filename=""):
+    """print out the entire content of a file to stdout using UTF8 encoding
+
+    Args:
+        filename (str): file to be printed
+
+    """
+    with open(filename, encoding='UTF8') as f:
+        print(f.read())
