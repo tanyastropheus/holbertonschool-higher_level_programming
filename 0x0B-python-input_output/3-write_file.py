71364a468789db7bed9bee5b69056f74055beb17
Tanya Kryukova <ttkryukova@gmail.com>
2017-09-08 03:28:07 +0000

    first commit

---
+++ 0x0B-python-input_output/3-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def write_file(filename="", text=""):
+    """count the number of lines in a text file
+
+    Args:
+        filename (str): file to be written
+        text (str): string to write to filename
+
+    Returns:
+        number of characters written.
+
+    """
+    with open(filename, 'w', encoding="UTF8") as f:  # create the file
+        f.write(text)
+        return f.write(text)
