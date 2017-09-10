dd52d31abe1cecd520a242b0089e2696661c3e23
Tanya Kryukova <ttkryukova@gmail.com>
2017-09-10 19:16:00 +0000

    refactoring code & added inline comment

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -3,16 +3,19 @@ def append_after(filename="", search_string="", new_string=""):
     with open(filename, 'r') as f:
         content = f.readlines()  # creating a list of lines
 
+    # inset lines in the copy since we cannot modify while iterating
     content_copy = list(content)
 
-    i, j = 1, 0
+    insert_index = 1
     for line in content:
         if search_string in line:
-            content_copy.insert(i + j, new_string)
-            j += 1  # accounting for the offset in the copy after insert
-        i += 1
+            content_copy.insert(insert_index, new_string)
+            insert_index += 1  # account for the offset in the copy after insert
+        insert_index += 1
 
+    # join lines back into text
     new_content = "".join(content_copy)
 
+    # rewrite new content into the file
     with open(filename, 'w') as f:
         f.write(new_content)
