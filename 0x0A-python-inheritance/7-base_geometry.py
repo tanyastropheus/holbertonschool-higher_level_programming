97c514885fe4998ff0828ca019ca834b2e0b2caa
Tanya Kryukova <ttkryukova@gmail.com>
2017-09-11 06:32:56 +0000

    used format string to print out exception messages

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -21,6 +21,6 @@ class BaseGeometry:
         """
 
         if type(value) != int:
-            raise TypeError(name + " must be an integer")
-        if  value <= 0:
-            raise ValueError(name + " must be greater than 0")
+            raise TypeError("{%s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{%s} must be greater than 0".format(name))
