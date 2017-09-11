a5099307a631258fc283a152d5e9ec5ccd00a58b
Tanya Kryukova <ttkryukova@gmail.com>
2017-09-11 06:41:59 +0000

    fixed typo

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -20,7 +20,7 @@ class BaseGeometry:
             ValueError: if value is less or equal to 0
         """
 
-        if type(int) != int:
+        if type(value) != int:
             raise TypeError(name + " must be an integer")
         if value <= 0:
             raise ValueError(name + " must be greater than 0")
