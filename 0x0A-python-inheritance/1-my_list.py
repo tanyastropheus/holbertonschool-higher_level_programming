b83d1ec1ad6ed2dd6c5b50c61162046f2e96b142
Tanya Kryukova <ttkryukova@gmail.com>
2017-09-11 05:18:10 +0000

    added documentation

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""contains MyList class inherited from the list class"""
+
+
 class MyList(list):
     def print_sorted(self):
+        """prints a sorted copy of the list"""
         print(sorted(self))
