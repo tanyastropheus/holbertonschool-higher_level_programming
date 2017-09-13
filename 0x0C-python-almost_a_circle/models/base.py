b81de411ca88c77f05ef22506d2f65985c09470f
Tanya Kryukova <ttkryukova@gmail.com>
2017-09-13 06:37:45 +0000

    added docstring

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
+"""Module contains class Base
+
+"""
+
+
 class Base:
+    """Base class for classes of other shapes."""
     __nb_objects = 0
+
     def __init__(self, id=None):
-        if id is  None:
+        if id is None:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
         else:
