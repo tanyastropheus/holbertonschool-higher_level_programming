49cbbcad3f6eed265034b668969c76f688bdabc5
Tanya Kryukova <ttkryukova@gmail.com>
2017-09-24 23:06:20 +0000

    adding public getter & setter for size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,17 +7,26 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """Produces a square based on given parameters."""
     def __init__(self, size, x=0, y=0, id=None):  # accepts arguments passed
-        """Initializing instances of class Rectangle.
+        """Initializing instances of class Square.
 
         Args:
-           width (int): width of the rectangle.
-           height (int): height of the rectangle.
-           x (int): horizontal (x-axis) offset of the rectangle.
-           y (int): vertical (y-axis) offset of the rectangle.
-           id (int): id of the Rectangle instance.
+           width (int): width of the square.
+           height (int): height of the square.
+           x (int): horizontal (x-axis) offset of the square.
+           y (int): vertical (y-axis) offset of the square.
+           id (int): id of the Square instance.
 
         """
         super().__init__(size, size, x, y, id)  # calls parent with arguments
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
 
     def __str__(self):
         return "[{:s}] ({:d}) {:d}/{:d} - {:d}".format(
