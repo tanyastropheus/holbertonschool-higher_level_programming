3f915b9402bdeaf7cd5368aee8285532deaebd92
Tanya Kryukova <ttkryukova@gmail.com>
2017-09-25 05:24:55 +0000

    saved file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -54,6 +54,7 @@ class Base:
            json_string (list): list of JSON string representation.
 
         """
-        if json_string is not None or != []:
-            return json.loads(json_string)
-        return "[]"
+        # not json_string -> check if it's empty
+        if not json_string or json_string is None:
+            return "[]"
+        return json.loads(json_string)
