27c2db10f3ee22306cb0781dd35fea7933d67635
Tanya Kryukova <ttkryukova@gmail.com>
2017-10-18 09:49:14 +0000

    changed column 1 name from 'name' to 'genre'

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,8 @@
 -- display all genres and the number of shows linked to each
 -- genres that do not have any shows linked will not be displayed
-SELECT tv_genres.name, COUNT(tv_show_genres.genre_id) AS number_shows
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_shows
        FROM tv_show_genres
        LEFT JOIN tv_genres
        ON tv_genres.id=tv_show_genres.genre_id
-GROUP BY tv_genres.name
+GROUP BY genre
 ORDER BY number_shows DESC;
