dfe11698eb44ede0c5043bfb4194f0b95b7b4b7b
Tanya Kryukova <ttkryukova@gmail.com>
2017-10-18 20:21:21 +0000

    reverted back to LEFT JOIN

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -3,6 +3,6 @@
 -- record is sorted first by tv show titles and then by genre id
 SELECT tv_shows.title, tv_show_genres.genre_id
        FROM tv_shows
-       INNER JOIN tv_show_genres
+       LEFT JOIN tv_show_genres
        	     ON tv_shows.id=tv_show_genres.show_id
        ORDER BY tv_shows.title, tv_show_genres.genre_id;
