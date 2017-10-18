40b95f3e31c56b4fc6a2ea01d8a3db196db76f16
Tanya Kryukova <ttkryukova@gmail.com>
2017-10-18 09:52:57 +0000

    added comments

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,7 +2,7 @@
 -- genres that do not have any shows linked will not be displayed
 SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_shows
        FROM tv_show_genres
-       LEFT JOIN tv_genres
+       LEFT JOIN tv_genres  -- only genres whose id exists in tv_show_genres.genre_id will be displayed
        ON tv_genres.id=tv_show_genres.genre_id
 GROUP BY genre
 ORDER BY number_shows DESC;
