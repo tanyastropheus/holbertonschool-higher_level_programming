5a0d028931e2070fb2f9cba7922409f5936fcfc0
Tanya Kryukova <ttkryukova@gmail.com>
2017-10-18 03:57:24 +0000

    specified database for table 'states'

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -3,6 +3,6 @@ SELECT id, name
        FROM hbtn_0d_usa.cities
        WHERE state_id =
        (SELECT id
-       	       FROM states
+       	       FROM hbtn_0d_usa.states
 	       WHERE name = 'California')
        ORDER BY cities.id ASC;
