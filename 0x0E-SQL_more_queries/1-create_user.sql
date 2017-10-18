a83339bbb7a9b2521ba3cf1b4472937eabb5b840
Tanya Kryukova <ttkryukova@gmail.com>
2017-10-18 00:09:02 +0000

    fixed incorrect command

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- create a user 'user_0d_1' and grant all privileges
 CREATE USER user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-CREATE ALL PRIVILEGES on * . * TO user_0d_1@localhost;
+GRANT ALL PRIVILEGES on * . * TO user_0d_1@localhost;
