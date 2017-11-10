6f6e393d88e73af473a3e0ff2af9290c9fdf8c75
Tanya Kryukova <ttkryukova@gmail.com>
2017-11-10 02:30:17 +0000

    added documentation and ensured code is not executed when imported

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
+'''module displays data entries starting with 'N'
+'''
+
 import MySQLdb
 import re
 from sys import argv
-# open database connection
-db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2],
-                     db=argv[3], charset="utf8")
 
-# prepare a cursor object using cursor() method
-cur = db.cursor()
+if __name__ == "__main__":
+    # open database connection
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
+    # prepare a cursor object using cursor() method
+    cur = db.cursor()
 
-# using MySQL regexp to select entries whose names start with 'N'
-cur.execute("SELECT * FROM states WHERE name REGEXP '^N' ORDER BY id ASC")
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-db.close()
+    # using MySQL regexp to select entries whose names start with 'N'
+    cur.execute("SELECT * FROM states WHERE name REGEXP '^N' ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    db.close()
