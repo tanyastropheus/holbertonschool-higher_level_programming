9b26075fa42a908b1565456198287e09ec8c80a9
Tanya Kryukova <ttkryukova@gmail.com>
2017-11-10 02:33:00 +0000

    added documentation and ensured code is not executed when imported

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
+
+'''module displays all data from the specified database'''
+
 import MySQLdb
 from sys import argv
-# open database connection
-db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2],
-                     db=argv[3], charset="utf8")
 
-# prepare a cursor object using cursor() method
-cur = db.cursor()
+if __name__ == "__main__":
+    # open database connection
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
 
-cur.execute("SELECT * FROM states ORDER BY id ASC")
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-db.close()
+    # prepare a cursor object using cursor() method
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+        cur.close()
+        db.close()
