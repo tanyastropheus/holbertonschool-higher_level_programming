0c3773e84aab24515b4c7af549e1f4b0d15ad179
Tanya Kryukova <ttkryukova@gmail.com>
2017-11-10 02:38:33 +0000

    added documentation and ensured code is not executed when imported

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
+
+'''module displays data entries that contain matching argument'''
+
 import MySQLdb
 import re
 from sys import argv
-# open database connection
-db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2],
-                     db=argv[3], charset="utf8")
 
-# prepare a cursor object using cursor() method
-cur = db.cursor()
+if __name__ == "__main__":
+    # open database connection
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
+    # prepare a cursor object using cursor() method
+    cur = db.cursor()
 
-# format WHERE with list of arguments and %s
-cur.execute("SELECT * FROM states WHERE name = %s", [argv[4]])
-data = cur.fetchone()
-print(data)
-cur.close()
-db.close()
+    # format WHERE with %s and a list of argument (substitution)
+    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC",
+                [argv[4]])
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    db.close()
