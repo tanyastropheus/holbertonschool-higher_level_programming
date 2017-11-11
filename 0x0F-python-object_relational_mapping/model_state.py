0e811f02ecdedf7870299641930e44781a4342dc
Tanya Kryukova <ttkryukova@gmail.com>
2017-11-11 01:55:50 +0000

    first commit

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''Create the 'states' table and the mapping to it'''
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
