954c4560ad5b1f7f78fc3fe407dba08f6a747fc1
Tanya Kryukova <ttkryukova@gmail.com>
2017-11-11 05:16:12 +0000

    first commit

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Contains State class mapped to states table in the DB"""
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sys import argv
+
+# allow to create mapping to the database
+Base = declarative_base()
+
+
+class State(Base):
+    """connect State objects with the table 'states' in the DB, or
+    create the table if it doesn't already exist in the DB"""
+
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+
+if __name__ == "__main__":
+    # set up connection to the database
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(argv[1], argv[2], argv[3]))
+
+    # binding ORM's handle to the database
+    Session = sessionmaker(bind=engine)
+
+    # set up connection to retrieve data from the DB
+    session = Session()
+
+    # print all State objects that contain the letter 'a'
+    try:
+        for instance in session.query(State).filter(
+                State.name.ilike('%a%')).order_by(State.id):
+            print("{}: {}".format(instance.id, instance.name))
+
+    except:
+        print("Nothing")
