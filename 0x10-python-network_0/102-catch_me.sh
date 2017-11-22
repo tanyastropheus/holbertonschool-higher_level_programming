46ed5b0f9887b3315c8d6a407c392b6bd5d40de8
Tanya Kryukova <ttkryukova@gmail.com>
2017-11-22 06:09:07 +0000

    first commit

---
+++ 0x10-python-network_0/102-catch_me.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# makes a request to 0.0.0.0:5000/catch_me
+# script should  cause the server to respond with a message containing You got me!
+curl -s -o /dev/null 0.0.0.0:5000/catch_me -w "You got me!\n"
