84c55bb4fec526bcdb4e73eab464e976a763517a
Tanya Kryukova <ttkryukova@gmail.com>
2017-11-29 06:51:32 +0000

    first commit

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''send a request to the URL & displays the body of response'''
+
+import urllib.request
+from sys import argv
+
+
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(argv[1]) as response:
+            content = response.read()
+            print(content.decode('utf-8'))
+    '''display HTTP status code'''
+    except urllib.error.HTTPError as e:
+        print("Error code:", e.code)
