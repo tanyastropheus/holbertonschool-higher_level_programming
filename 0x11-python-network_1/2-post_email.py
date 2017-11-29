f37984ec217c7b2da46999d5adf9b75734cd8f3f
Tanya Kryukova <ttkryukova@gmail.com>
2017-11-29 05:05:43 +0000

    will not execute if imported

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,13 +5,15 @@ import urllib.request
 import urllib.parse
 from sys import argv
 
-'''data to POST to the server'''
-data = urllib.parse.urlencode({'email': argv[2]})
-data = data.encode('utf-8')
 
-try:
-    with urllib.request.urlopen(argv[1], data) as response:
-        content = response.read()
-        print(content.decode('utf-8'))
-except:
-    pass
+if __name__ == "__main__":
+    '''data to POST to the server'''
+    data = urllib.parse.urlencode({'email': argv[2]})
+    data = data.encode('ascii')
+
+    try:
+        with urllib.request.urlopen(argv[1], data) as response:
+            content = response.read()
+            print(content.decode('utf-8'))
+    except:
+        pass
