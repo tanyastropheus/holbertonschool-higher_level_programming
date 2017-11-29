a05dbb4f0201c9fe4c9eabbff2c5e20214d676d7
Tanya Kryukova <ttkryukova@gmail.com>
2017-11-29 07:42:35 +0000

    deleted Request object to test checker

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -6,9 +6,8 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    req = urllib.request.Request(argv[1])
     try:
-        with urllib.request.urlopen(req) as response:
+        with urllib.request.urlopen(argv[1]) as response:
             content = response.read()
             print(content.decode('utf-8'))
     except urllib.error.HTTPError as e:
