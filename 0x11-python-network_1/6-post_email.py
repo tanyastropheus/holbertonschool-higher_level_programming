e616d34fd49a0ede2064c6bfa6da380ff7eafdd9
Tanya Kryukova <ttkryukova@gmail.com>
2017-12-01 21:14:03 +0000

    first commit

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''send a POST request with data & displays the body of the response'''
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    try:
+        r = requests.post(argv[1], data = {'email': argv[2]})
+        print(r.text)
+    except:
+        pass
