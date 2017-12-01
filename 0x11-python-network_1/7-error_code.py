f5985efb2bc3e19eb53d37e6e5db2b06a6b1db76
Tanya Kryukova <ttkryukova@gmail.com>
2017-12-01 22:04:14 +0000

    added comments

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,7 +7,7 @@ from sys import argv
 if __name__ == "__main__":
     r = requests.get(argv[1])
     try:
-        r.raise_for_status()
+        r.raise_for_status()  # rase_for_status() raises 4XX/5XX errors
         print(r.text)
     except requests.exceptions.HTTPError:
         print("Error code:", r.status_code)
