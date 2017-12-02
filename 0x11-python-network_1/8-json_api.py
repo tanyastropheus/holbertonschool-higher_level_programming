d9a0c9c75ad9ef926e4470bc737ba37c8612900e
Tanya Kryukova <ttkryukova@gmail.com>
2017-12-02 00:45:15 +0000

    catching Not Valid json

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,14 +8,16 @@ if __name__ == "__main__":
     if len(argv) == 1:
         r = requests.post('http://0.0.0.0:5000/search_user', data={'q': ""})
     elif len(argv) > 1:
-        r = requests.post(
-            'http://0.0.0.0:5000/search_user', data={'q': argv[1]})
+        r = requests.post('http://0.0.0.0:5000/search_user', data={'q': argv[1]})
 
     content = r.json()
-    if content:
-        try:
-            print("[{}] {}".format(content['id'], content['name']))
-        except:
-            print("Not a valid JSON")
-    else:
+    id_num = content.get('id')
+    name = content.get('name')
+
+    if id_num is None or name is None:
         print("No result")
+    else:
+        try:
+            print("[{}] {}".format(id_num, name))
+        except ValueError:
+            print("Not a valid JSON")
