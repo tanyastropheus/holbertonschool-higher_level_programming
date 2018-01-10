021364c1e93fcaace1f33366b0c3d898177abf71
Tanya Kryukova <ttkryukova@gmail.com>
2018-01-10 05:59:22 +0000

    fixed typo in print()

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -11,12 +11,9 @@ module.exports = class Rectangle {
   }
 
   print () {
-    console.log(this.w);
     let i = 0;
-    console.log(this.h);
-    while (i < this.h) {
-      console.log('X'.repeat(this.w));
-      console.log('inside function');
+    while (i < this.height) {
+      console.log('X'.repeat(this.width));
       i++;
     }
   }
