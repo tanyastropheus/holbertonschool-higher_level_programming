e2faa2c18becc13bae0350f037cc43d53214246e
Tanya Kryukova <ttkryukova@gmail.com>
2018-01-11 00:28:42 +0000

    first commit

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+var fs = require('fs');
+
+fs.readFile(process.argv[2], 'utf-8', function (err, data) {
+  if (err) {
+    return console.error(err);
+  }
+  console.log(data);
+});
