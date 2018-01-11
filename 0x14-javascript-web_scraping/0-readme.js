6272999e764a43ab27bee0c839cd9acba40292b9
Tanya Kryukova <ttkryukova@gmail.com>
2018-01-11 00:34:42 +0000

    formatted output & fixed style

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-let  fs = require('fs');
+let fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf-8', function (err, data) {
   if (err) {
     return console.error(err);
   }
-  console.log(data);
+  process.stdout.write(data);
 });
