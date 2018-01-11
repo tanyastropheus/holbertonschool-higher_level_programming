010cf91955ec0d1b87aad732a5ac6b1cd58b25e6
Tanya Kryukova <ttkryukova@gmail.com>
2018-01-11 00:48:29 +0000

    first commit

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+let fs = require('fs');
+
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8', function (err) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(process.argv[3]);
+});
