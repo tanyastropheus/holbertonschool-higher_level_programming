15b139f59551412d7259a0f03d99d020020abb59
Tanya Kryukova <ttkryukova@gmail.com>
2018-01-11 07:34:01 +0000

    produces the right output using a hack

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -11,38 +11,36 @@ const options = {
   }
 };
 
-function taskCompleted(task) {
-  if (task['completed']) {
-    return true;
-  }
-};
-
-
 request(options, function (err, response, body) {
   if (err) {
     return console.log(err);
   }
-  let json = JSON.parse(body);  // list of dicts
-//  console.log("inside:", json);
+  let json = JSON.parse(body); // list of dicts
+  //  console.log("inside:", json);
   let userTaskNum = {};
-  let tasksCompleted = json.filter(function(tasks) {
+  let tasksCompleted = json.filter(function (tasks) {
     if (tasks['completed']) {
       return true;
-      }
+    }
   });
-//  console.log(newList);
+  //  console.log(newList);
   let i = 0;
   let userID = 1;
   let count = 0;
   while (i < tasksCompleted.length) {
-    if (tasksCompleted[i]['userId'] == userID) {
+    if (tasksCompleted[i]['userId'] === userID) {
       count++;
+      //      console.log("user ID:", userID);
+      //      console.log("count:", count);
     } else {
       userTaskNum[userID] = count;
       userID++;
-      count = 0;
+      count = 1;
     }
     i++;
+    if (i === tasksCompleted.length) {
+      userTaskNum[userID] = count;
+    }
   }
   console.log(userTaskNum);
 });
