78aa537958fac50d9671b782a6817f0710f6d121
Tanya Kryukova <ttkryukova@gmail.com>
2018-01-11 00:40:07 +0000

    print error to console instead of console.err

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,7 +4,7 @@ let fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf-8', function (err, data) {
   if (err) {
-    return console.error(err);
+    return console.log(err);
   }
   process.stdout.write(data);
 });
